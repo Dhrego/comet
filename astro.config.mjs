95c198ed70b071d81d78e0c3c498f0fd07317e43
Andrea <andrea.dick3@gmail.com>
2024-06-17 15:31:31 +0200

    loader bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [react()],
-  // adapter: vercel(),
+  adapter: vercel(),
 });
