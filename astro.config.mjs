17f2947f6a1583f873ee8620c5d3117a7f3a010a
Andrea <andrea.dick3@gmail.com>
2024-06-17 14:05:52 +0200

    added DreamyGradients

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel(),
+  integrations: [react()],
+  // adapter: vercel(),
 });
