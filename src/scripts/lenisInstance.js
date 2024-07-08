99eef2bdb9e3c2c6f1f5e276dffd421e6bde40ce
Andrea <andrea.dick3@gmail.com>
2024-07-08 11:26:26 +0200

    lenis fix

---
+++ src/scripts/lenisInstance.js
@@ -0,0 +1,24 @@
+import Lenis from "lenis";
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+function disableLenis() {
+  lenis.stop();
+}
+
+function enableLenis() {
+  lenis.start();
+}
+
+export { lenis, disableLenis, enableLenis };
