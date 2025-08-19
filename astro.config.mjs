468ed9c749eccef06b7c094dd9316c01af2d63ce
Andrea <andrea.dick3@gmail.com>
2025-08-19 17:21:18 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.dhrego.com",
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
